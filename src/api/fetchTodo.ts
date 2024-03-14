43002dd4b9af042f453642a6b0b13253dd6a93bb
gkstnadl <gkstnadl12@naver.com>
2024-03-15 03:00:19 +0900

    Feat: Json서버 구현 완료

---
+++ src/api/fetchTodo.ts
@@ -0,0 +1,25 @@
+import { TodoType } from "@/types";
+
+const url = "http://localhost:4000/todos";
+
+export const fetchTodo = async () => {
+  const res = await fetch(`${url}`);
+  const data = await res.json();
+  return data;
+};
+
+export const addTodo = async (newTodo: TodoType) => {
+  await fetch(`${url}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newTodo),
+  });
+};
+
+export const deleteTodo = async (id: string) => {
+  await fetch(`${url}/${id}`, {
+    method: "DELETE",
+  });
+};
--- src/app/todos-ssr/page.tsx
+++ src/app/todos-ssr/page.tsx
@@ -1,7 +1,56 @@
 import React from "react";
+import Link from "next/link";
+import { TodoType } from "@/types";
+import InputForm from "@/components/InputForm";
 
-const page = () => {
-  return <div>todos-ssr Page</div>;
+const page = async () => {
+  const res = await fetch("http://localhost:4000/todos", {
+    cache: "no-cache",
+  });
+  const todolist: TodoType[] = await res.json();
+
+  return (
+    <div>
+      <h1>TodoList</h1>
+      <InputForm />
+      <div>
+        <h2>Working🔥</h2>
+        {todolist
+          .filter((item) => {
+            return item.isDone === false;
+          })
+          .map((item) => {
+            return (
+              <div key={item.id}>
+                <Link href={`/todos-ssr/${item.id}`}>
+                  <h3>{item.title}</h3>
+                  <p>{item.contents}</p>
+                </Link>
+                <button>삭제하기</button>
+                <button>{item.isDone ? "취소하기" : "완료하기"}</button>
+              </div>
+            );
+          })}
+      </div>
+      <div>
+        <h2>Done🎉</h2>
+        {todolist
+          .filter((item) => {
+            return item.isDone === true;
+          })
+          .map((item) => {
+            return (
+              <div key={item.id}>
+                <Link href={`/todos-ssr/${item.id}`}>
+                  <h3>{item.title}</h3>
+                  <p>{item.contents}</p>
+                </Link>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 };
 
 export default page;
